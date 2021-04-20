38c994424e90a6cf7539d70f8d885b34f04b9df4
Alexandr Honcharenko <alex.devios93@gmail.com>
2021-04-20 13:04:03 +0300

    add battery func

--- batteryTest/ViewController.swift
+++ batteryTest/ViewController.swift
@@ -9,18 +9,54 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var button: NSButton?
+    @IBOutlet weak var label: NSTextField?
 
-        // Do any additional setup after loading the view.
+    @IBAction func pressButton(_ sender: Any) {
+        let batteryInfo = BatteryInfo()
+        //
+        print("isBatteryCharging: ", batteryInfo.isBatteryCharging())
+        print("batteryLevel: ", batteryInfo.batteryLevel())
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
 
+import IOKit.ps
+class BatteryInfo {
+
+    //used only for iOS and ignored for macOS
+    var isBatteryMonitoringEnabled: Bool = false
+
+    func isBatteryCharging() -> Bool {
+        if let batteryInfo = self.batteryInfo(),
+            let isCharging = batteryInfo[kIOPSIsChargingKey] as? Bool {
+            return isCharging
+        }
+        return true
+    }
+
+    func batteryLevel() -> Int {
+        if let batteryInfo = self.batteryInfo(),
+            let capacity = batteryInfo[kIOPSCurrentCapacityKey] as? Int {
+            return capacity
+        }
+        return 100
+    }
+
+    private func batteryInfo() -> NSDictionary? {
+
+        // Take a snapshot of all the power source info
+        guard let snapshot = IOPSCopyPowerSourcesInfo()?.takeRetainedValue() else { return nil }
+//        print(snapshot)
+
+        // Pull out a list of power sources
+        guard let sources: NSArray = IOPSCopyPowerSourcesList(snapshot)?.takeRetainedValue() else { return nil }
+//        print(sources)
+
+        // For each power source...
+        guard let powerSource = sources.lastObject else { return nil }
+//        print(powerSource)
+        guard let info: NSDictionary = IOPSGetPowerSourceDescription(snapshot, powerSource as CFTypeRef)?.takeUnretainedValue() else { return nil }
+//        print(info)
+        return info
+    }
+}
