5ca0e0a38abef8cabe672418437e5670dc0af701
Alexandr Honcharenko <alex.devios93@gmail.com>
2021-04-20 12:54:12 +0300

    Initial Commit

---
+++ batteryTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  batteryTest
+//
+//  Created by Alexandr Honcharenko on 20.04.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
